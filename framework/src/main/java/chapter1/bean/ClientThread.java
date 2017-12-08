ecb1ea75fdbe915a5de0053b7d3af2fbd47e7743
chape <chape1989@gmail.com>
2017-12-08 18:39:03 +0800

    first aop

---
+++ framework/src/main/java/chapter1/bean/ClientThread.java
@@ -0,0 +1,18 @@
+package chapter1.bean;
+
+/**
+ * Created by ChaoChao on 08/12/2017.
+ */
+public class ClientThread extends Thread {
+    private Sequence sequence;
+
+    public ClientThread(Sequence sequence){
+        this.sequence = sequence;
+    }
+    @Override
+    public void run(){
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Thread.currentThread().getName() + "=>" + sequence.getNumber());
+        }
+    }
+}
---
+++ framework/src/main/java/chapter1/bean/Sequence.java
@@ -0,0 +1,8 @@
+package chapter1.bean;
+
+/**
+ * Created by ChaoChao on 08/12/2017.
+ */
+public interface Sequence {
+    int getNumber();
+}
